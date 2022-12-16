d40ffc909025cbb04d035e9681baea403ad947e2
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-16 22:56:07 +0200

    it checks if there is items in List<Geofence> to retrieve the first Geofence RequestId

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -5,9 +5,9 @@ import android.content.Intent
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
+import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.sendNotification
 import kotlinx.coroutines.*
@@ -36,10 +36,11 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
         sendNotification(GeofencingEvent.fromIntent(intent).triggeringGeofences)
     }
 
-    //TODO: get the request id of the current geofence
     private fun sendNotification(triggeringGeofences: List<Geofence>) {
-        val requestId = ""
-
+        val requestId = when {
+            triggeringGeofences.isNotEmpty() -> triggeringGeofences[0].requestId
+            else -> return
+        }
         //Get the local repository instance
         val remindersLocalRepository: ReminderDataSource by inject()
 //        Interaction to the repository has to be through a coroutine scope
