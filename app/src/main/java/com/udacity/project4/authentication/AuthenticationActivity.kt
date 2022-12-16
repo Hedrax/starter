84428c053f6809d63e17bee5f2f24d825e997e34
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-16 19:49:19 +0200

    -Adding Companion object only to hold two constants { 1- SIGN_IN_CODE to keep track of the sign in response if it matches the Sign in code that we based in when requesting 2- TAG only for log debug statement to trace error }

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,15 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,6 +17,10 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    companion object{
+        const val SIGN_IN_CODE = 132
+        private const val TAG = "AuthenticationActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
@@ -19,6 +30,7 @@ class AuthenticationActivity : AppCompatActivity() {
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+
 
     }
 }
