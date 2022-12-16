0550c25008e76115331beca7bcf21b808872f0fa
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-17 00:50:53 +0200

    Given - using the builder we implemented we create pending intent and geoRequest

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -11,6 +11,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
@@ -64,6 +65,8 @@ class SaveReminderFragment : BaseFragment() {
             val geoId: String = UUID.randomUUID().toString()
             val geofence: Geofence = geoBuilder.buildGeofence(geoId,latLng,radius,
                 Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_EXIT)
+            val pendingIntent: PendingIntent? = geoBuilder.getPendingIntent()
+            val geofenceRequest: GeofencingRequest = geoBuilder.getGeofencingRequest(geofence)
 
         }
         catch (e:Exception){
