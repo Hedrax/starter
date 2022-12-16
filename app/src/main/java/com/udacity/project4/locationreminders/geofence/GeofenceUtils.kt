1a83a5aab19f237a4f02037f5ae8b404d8d4e065
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-16 22:05:47 +0200

    adding a fn that returns a geofence after building it the fn takes (ID,LatLng, radios, and type ) as attributes of the built geofence

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceUtils.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceUtils.kt
@@ -2,7 +2,20 @@ package com.udacity.project4.locationreminders.geofence
 
 import android.content.Context
 import android.content.ContextWrapper
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.maps.model.LatLng
 
 class GeofenceUtils(context: Context?) {
-    //TODO - geofence - pendingIntent - handleRequest
+    //TODO - pendingIntent - handleRequest
+
+    fun buildGeofence(ID: String, latLng: LatLng, rad: Float, type: Int): Geofence {
+        return Geofence.Builder()
+            .setCircularRegion(latLng.latitude, latLng.longitude, rad)
+            .setRequestId(ID)
+            .setTransitionTypes(type)
+            .setLoiteringDelay(6000)
+            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+            .build()
+    }
+
 }
