3f8a8f8c3dab3773c2e92ea15aa6e6bd8f0c08fd
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-16 22:00:18 +0200

    adding a class to help dealing with geofence operations

---
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceUtils.kt
@@ -0,0 +1,8 @@
+package com.udacity.project4.locationreminders.geofence
+
+import android.content.Context
+import android.content.ContextWrapper
+
+class GeofenceUtils(context: Context?) {
+    //TODO - geofence - pendingIntent - handleRequest
+}
