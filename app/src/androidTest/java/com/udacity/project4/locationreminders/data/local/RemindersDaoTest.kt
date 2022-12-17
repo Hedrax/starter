ba2f3140dd5847155fdcc4b7dfb7fe45e72ea2c2
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-17 04:30:38 +0200

    Testing getReminders

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -7,8 +7,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi;
@@ -16,8 +14,8 @@ import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.After
-import org.junit.Test
+import org.junit.*
+import kotlin.random.Random
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -36,4 +34,25 @@ class RemindersDaoTest {
     }
     @After
     fun dataBaseClosure() = db.close()
+    @Test
+    fun getRemindersTesting() = runBlockingTest {
+        //GIVEN a reminder into the database
+        val reminder = ReminderDTO(
+            "title",
+            "description",
+            "location",
+            (0..360).random().toDouble(),
+            (0..360).random().toDouble())
+        db.reminderDao().saveReminder(reminder)
+        //When extracting the reminder of the db
+        val remindersLst = db.reminderDao().getReminders()
+        //THEN the lst that represents database has only one item in it
+        Assert.assertThat(remindersLst.size, `is`(1))
+        Assert.assertThat(remindersLst[0].id, `is`(reminder.id))
+        Assert.assertThat(remindersLst[0].location, `is`(reminder.location))
+        Assert.assertThat(remindersLst[0].latitude, `is`(reminder.latitude))
+        Assert.assertThat(remindersLst[0].longitude, `is`(reminder.longitude))
+        Assert.assertThat(remindersLst[0].title, `is`(reminder.title))
+        Assert.assertThat(remindersLst[0].description, `is`(reminder.description))
+    }
 }
