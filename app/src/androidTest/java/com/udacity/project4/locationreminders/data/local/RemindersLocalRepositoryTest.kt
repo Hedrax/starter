f7b26a6c7d4a60b688c2b6e4166d576242ee78de
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-21 21:44:26 +0200

    initializing localDataSource through our fake data class and also initializing reminderRepository through our fakeDAOclass

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -5,6 +5,8 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.udacity.project4.fake.FakeDataSource
+import com.udacity.project4.fake.FakeReminderDAO
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.Dispatchers
@@ -18,6 +20,8 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.UUID
+import kotlin.random.Random
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -26,5 +30,22 @@ import org.junit.runner.RunWith
 class RemindersLocalRepositoryTest {
 
 //    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    private val Reminder1 = ReminderDTO("Title1", "Description1","Location 1", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val Reminder2 = ReminderDTO("Title2", "Description2","Location 2", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val Reminder3 = ReminderDTO("Title3", "Description3","Location 3", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val ReminderNew = ReminderDTO("TitleNew", "DescriptionNew","Location New", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val localReminders = listOf(Reminder1,Reminder2,Reminder3).sortedBy { it.id }
+    private val newReminders = listOf(ReminderNew).sortedBy { it.id }
 
+    private lateinit var remindersLocalDataSource: FakeDataSource
+    private lateinit var remindersRepository: RemindersLocalRepository
+    private lateinit var reminderDao: FakeReminderDAO
+    @Before
+    fun repoCreation(){
+        remindersLocalDataSource = FakeDataSource(localReminders.toMutableList())
+        reminderDao = FakeReminderDAO()
+        remindersRepository = RemindersLocalRepository(
+            reminderDao,
+            Dispatchers.Unconfined)
+    }
 }
