19bd6a5fda92263bb787b3f33b270bae5139891c
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-21 23:40:29 +0200

    setup we may use to test the DAO same as localRepoTest

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -23,4 +23,13 @@ class RemindersDaoTest {
     @get:Rule
     var instantExecutor = InstantTaskExecutorRule()
 
+    private val reminder1 = ReminderDTO("Title1", "Description1","Location 1", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val reminder2 = ReminderDTO("Title2", "Description2","Location 2", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val reminder3 = ReminderDTO("Title3", "Description3","Location 3", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val reminderNew = ReminderDTO("TitleNew", "DescriptionNew","Location New", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val localReminders = listOf(reminder1,reminder2,reminder3).sortedBy { it.id }
+    private val newReminders = listOf(reminder1,reminder2,reminder3,reminderNew).sortedBy { it.id }
+
+
+
 }
