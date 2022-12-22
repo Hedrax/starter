f8b4fd3c6f99b8c8c72e69ee440e2705bde411b1
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-22 16:12:56 +0200

    initializing the dataSourceVariable and adding test for liveData

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,9 +1,19 @@
 package com.udacity.project4.locationreminders.reminderslist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -12,9 +22,24 @@ class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
 
-    @Rule
+    @get:Rule
     var instantExecutor = InstantTaskExecutorRule()
 
-    val position = ReminderDataItem("Title","Description","Location",(0..100).random().toDouble(),(0..100).random().toDouble())
-
+    val exampleReminder = ReminderDTO("Title","Description","Location",(0..100).random().toDouble(),(0..100).random().toDouble())
+    lateinit var dataSource: FakeDataSource
+    @Before
+    fun setup(){
+        dataSource = FakeDataSource()
+    }
+    @Test
+    fun liveData_testingSaveFn() = runBlockingTest {
+        //Given a newly generated viewModel
+        val remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(),dataSource)
+        //When adding new reminder
+        dataSource.saveReminder(exampleReminder)
+        remindersListViewModel.loadReminders()
+        val reminders = remindersListViewModel.remindersList.getOrAwaitValue()
+        //Then the newReminder is triggered
+        assertThat(reminders.isEmpty(),`is`(false))
+    }
 }
