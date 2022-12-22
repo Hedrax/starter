691e0eaff5fe1ead0d55ff7e0acb8458a43301a9
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-22 17:13:50 +0200

    testing LiveData after setting position by some fake data

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,13 +1,20 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.test.core.app.ApplicationProvider
+import org.hamcrest.CoreMatchers.`is`
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.assertThat
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -21,4 +28,15 @@ class SaveReminderViewModelTest {
     lateinit var saveReminderViewModel: SaveReminderViewModel
     lateinit var dataSource: FakeDataSource
 
+    @Test
+    fun liveData_settingLiveDataVarsOfVm(){
+        //Given a fresh viewModel
+        dataSource = FakeDataSource()
+        saveReminderViewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(),dataSource)
+        //When setting Title, position LiveData Variables
+        saveReminderViewModel.setPositionReminder(exampleReminder)
+        //Then title value should've changed
+        val title = saveReminderViewModel.title.getOrAwaitValue()
+        assertThat(title,`is`(exampleReminder.title))
+    }
 }
