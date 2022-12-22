e7a1a03287b1fa1388511bc9c1748496376041b7
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-22 02:32:07 +0200

    AutoCloseKoin to close koin after each test and declaring variables will be useful in upcoming usage

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -12,10 +12,15 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.fake.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.Koin
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.KoinTest
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
@@ -23,9 +28,17 @@ import org.mockito.Mockito.verify
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
-class ReminderListFragmentTest {
+class ReminderListFragmentTest :AutoCloseKoinTest(){
 
 //    TODO: test the navigation of the fragments.
 //    TODO: test the displayed data on the UI.
 //    TODO: add testing for the error messages.
+
+    private lateinit var dataSource: FakeDataSource
+    private lateinit var ViewModel: RemindersListViewModel
+    private val reminder1 = ReminderDTO("Title1", "Description1","Location 1", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val reminder2 = ReminderDTO("Title2", "Description2","Location 2", (0..360).random().toDouble(),(0..360).random().toDouble())
+    private val reminder3 = ReminderDTO("Title3", "Description3","Location 3", (0..360).random().toDouble(),(0..360).random().toDouble())
+
+
 }
