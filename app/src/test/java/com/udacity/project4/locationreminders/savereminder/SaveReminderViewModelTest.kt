40c51e0da7d16494311c724a915795a3e9010978
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-22 16:40:42 +0200

    adding rule

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,17 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
-
-    //TODO: provide testing to the SaveReminderView and its live data objects
-
+    @get:Rule
+    var instantExecutor = InstantTaskExecutorRule()
 
 }
