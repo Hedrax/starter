be8333332bb3bbd9ce47fddf2deb5766eda564cd
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-22 14:38:06 +0200

    adding rule

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -9,6 +11,9 @@ import org.junit.runner.RunWith
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
+
+    @Rule
+    var instantExecutor = InstantTaskExecutorRule()
 
     val position = ReminderDataItem("Title","Description","Location",(0..100).random().toDouble(),(0..100).random().toDouble())
 
