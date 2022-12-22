720dc8897d66150c10b6e93dde5e2846c8a2e3a0
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-22 17:28:22 +0200

    errorTest

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -6,12 +6,14 @@ import androidx.lifecycle.LiveData
 import androidx.test.core.app.ApplicationProvider
 import org.hamcrest.CoreMatchers.`is`
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.getOrAwaitValue
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.Matcher
 import org.junit.Assert.assertThat
 import org.junit.Rule
 import org.junit.Test
@@ -38,5 +40,17 @@ class SaveReminderViewModelTest {
         //Then title value should've changed
         val title = saveReminderViewModel.title.getOrAwaitValue()
         assertThat(title,`is`(exampleReminder.title))
+    }
+
+    @Test
+    fun errorTesting_whenSavingWithNullValues(){
+        //Given a fresh viewModel
+        dataSource = FakeDataSource()
+        saveReminderViewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(),dataSource)
+        //Given Null values
+        assertThat(saveReminderViewModel.title.getOrAwaitValue().isNullOrBlank(),`is`(true))
+        //Then It should print out error
+        saveReminderViewModel.save()
+        assertThat(saveReminderViewModel.showSnackBarInt.getOrAwaitValue() == R.string.missing_title , `is`(true))
     }
 }
