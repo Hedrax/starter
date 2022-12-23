edbafce451f8a94daa623dbc2d8f387c85a8c885
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-23 02:08:12 +0200

    rewriting Fake dataSource

--- app/src/androidTest/java/com/udacity/project4/fake/FakeDataSource.kt
+++ app/src/androidTest/java/com/udacity/project4/fake/FakeDataSource.kt
@@ -4,20 +4,6 @@ import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
-class FakeDataSource(private var reminderDTOS: MutableList<ReminderDTO>? = mutableListOf()) :
-    ReminderDataSource {
-    override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        reminderDTOS?.let { return Result.Success(it) }
-        return Result.Error("No reminders found")
-    }
-    override suspend fun saveReminder(reminder: ReminderDTO){
-        reminderDTOS?.add(reminder)
-    }
-    override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        reminderDTOS?.firstOrNull { it.id == id }?.let { return Result.Success(it) }
-        return Result.Error("No reminders found")
-    }
-    override suspend fun deleteAllReminders() {
-        reminderDTOS = mutableListOf()
-    }
+class FakeDataSource(){
+
 }
