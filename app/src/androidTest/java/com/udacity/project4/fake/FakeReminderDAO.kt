01a1e1006cb6e1746c215949f985d5a51b0f1957
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-23 02:32:30 +0200

    it inheritance our original RemindersDAO class and it will ask to implement it's members

--- app/src/androidTest/java/com/udacity/project4/fake/FakeReminderDAO.kt
+++ app/src/androidTest/java/com/udacity/project4/fake/FakeReminderDAO.kt
@@ -4,6 +4,21 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.local.RemindersDao
 
 class FakeReminderDAO : RemindersDao {
+    override suspend fun getReminders(): List<ReminderDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getReminderById(reminderId: String): ReminderDTO? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllReminders() {
+        TODO("Not yet implemented")
+    }
 
 
 }
