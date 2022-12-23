9362209f227c77bcadfbd6a6156ee2ffeefcae1e
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-23 02:09:56 +0200

    defining a mutable list that will represent the data container instead of actually implement dataSource

--- app/src/androidTest/java/com/udacity/project4/fake/FakeDataSource.kt
+++ app/src/androidTest/java/com/udacity/project4/fake/FakeDataSource.kt
@@ -4,6 +4,7 @@ import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
-class FakeDataSource(){
+class FakeDataSource(private var reminderDTOS: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
 
 }
