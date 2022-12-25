efffc302ecb25e7152cf53ebbab609d517660593
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-25 23:37:58 +0200

    updating the fake data Source of the Local tests to be such as instrumented tests

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,18 +4,17 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource (private var reminderDTOS: MutableList<ReminderDTO>? = mutableListOf()) :
+class FakeDataSource (private var reminderDTOS: MutableList<ReminderDTO> = mutableListOf()) :
     ReminderDataSource {
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        reminderDTOS?.let { return Result.Success(it) }
-        return Result.Error("No reminders found")
+        reminderDTOS.let { return Result.Success(it) }
     }
     override suspend fun saveReminder(reminder: ReminderDTO){
-        reminderDTOS?.add(reminder)
+        reminderDTOS.add(reminder)
     }
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        reminderDTOS?.firstOrNull { it.id == id }?.let { return Result.Success(it) }
+        reminderDTOS.firstOrNull { it.id == id }?.let { return Result.Success(it) }
         return Result.Error("No reminders found")
     }
     override suspend fun deleteAllReminders() {
