69e36573281007154df2ced372095ce520ef2fac
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-25 19:38:44 +0200

    making the fake data source nonNullable type to match with the original data source

--- app/src/androidTest/java/com/udacity/project4/fake/FakeDataSource.kt
+++ app/src/androidTest/java/com/udacity/project4/fake/FakeDataSource.kt
@@ -5,19 +5,19 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 import net.bytebuddy.asm.Advice.Return
 
-class FakeDataSource(private var reminderDTOS: MutableList<ReminderDTO>? = mutableListOf()) :
+class FakeDataSource(private var reminderDTOS: MutableList<ReminderDTO> = mutableListOf()) :
     ReminderDataSource {
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        reminderDTOS?.let { return Result.Success(it) }
+        reminderDTOS.let { return Result.Success(it) }
         return Result.Error("No reminders found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        reminderDTOS?.add(reminder)
+        reminderDTOS.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        reminderDTOS?.firstOrNull(){it.id == id}?.let{return Result.Success(it) }
+        reminderDTOS.firstOrNull(){it.id == id}?.let{return Result.Success(it) }
         return Result.Error("No reminders found")
     }
 
