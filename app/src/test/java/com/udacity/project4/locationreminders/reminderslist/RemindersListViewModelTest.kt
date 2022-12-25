4c96de1b165833d00b726d1066b43868dabf385c
Hedrax <alhossien.waly@ejust.edu.eg>
2022-12-25 23:51:36 +0200

    setting test Coroutine dispatcher in order to observe the loading state + Test to implement the absorption

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -6,10 +6,16 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.MainCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -18,15 +24,24 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+    private val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
 
     @get:Rule
     var instantExecutor = InstantTaskExecutorRule()
 
-    val exampleReminder = ReminderDTO("Title","Description","Location",(0..100).random().toDouble(),(0..100).random().toDouble())
-    lateinit var dataSource: FakeDataSource
+    private val exampleReminder = ReminderDTO("Title","Description","Location",(0..100).random().toDouble(),(0..100).random().toDouble())
+    private lateinit var dataSource: FakeDataSource
+    @ExperimentalCoroutinesApi
     @Before
     fun setup(){
         dataSource = FakeDataSource()
+            Dispatchers.setMain(testDispatcher)
+    }
+    @ExperimentalCoroutinesApi
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
     }
     @Test
     fun liveData_testingSaveFn() = runBlockingTest {
@@ -42,11 +57,23 @@ class RemindersListViewModelTest {
     @Test
     fun errorMessage_WhenNullDataSource() = runBlockingTest {
         //Given a newly generated viewModel and an empty dataSource
-        dataSource = FakeDataSource(null)
+        dataSource = FakeDataSource()
         val remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(),dataSource)
         //when updating reminders
         remindersListViewModel.loadReminders()
         //Then returns error message in SnackBar
         assertThat(remindersListViewModel.showSnackBar.getOrAwaitValue(),`is`("No reminders found"))
     }
+    @Test
+    fun checkingLoading_whenSavingReminder() = runBlockingTest{
+        val reminderListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+        //Given a dataSource with some elements in it
+        dataSource.saveReminder(exampleReminder)
+        //when we add them and pause dispatcher in order to check for loading
+        testDispatcher.pauseDispatcher()
+        reminderListViewModel.loadReminders()
+        assertThat(reminderListViewModel.showLoading.getOrAwaitValue(), `is`(true))
+    }
+
+
 }
